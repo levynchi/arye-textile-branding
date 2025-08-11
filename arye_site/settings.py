6fcedaa0b43463d32b259abe80af2acd00e6b845
lior levy <levynchi@gmail.com>
2025-08-11 15:31:10 +0300

    Feat: footer icons + banner + gallery layout updates

--- arye_site/settings.py
+++ arye_site/settings.py
@@ -116,10 +116,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR / 'icons_logo',  # SVG social icons
+]
 STATIC_ROOT = BASE_DIR / 'staticfiles_build'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media (uploaded images)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
