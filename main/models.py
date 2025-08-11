e85d907feb25fdf179fbbb17352346f549b0e9d2
lior levy <levynchi@gmail.com>
2025-08-11 13:33:45 +0300

    Feature: gallery model + template placeholders and fix template syntax

--- main/models.py
+++ main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Gallery(models.Model):
+	"""Simple gallery of up to six images for home page blocks."""
+	title = models.CharField(max_length=120, blank=True, help_text="Optional overall gallery title")
+	image1 = models.ImageField(upload_to="gallery/", blank=True, null=True)
+	caption1 = models.CharField(max_length=120, blank=True)
+	image2 = models.ImageField(upload_to="gallery/", blank=True, null=True)
+	caption2 = models.CharField(max_length=120, blank=True)
+	image3 = models.ImageField(upload_to="gallery/", blank=True, null=True)
+	caption3 = models.CharField(max_length=120, blank=True)
+	image4 = models.ImageField(upload_to="gallery/", blank=True, null=True)
+	caption4 = models.CharField(max_length=120, blank=True)
+	image5 = models.ImageField(upload_to="gallery/", blank=True, null=True)
+	caption5 = models.CharField(max_length=120, blank=True)
+	image6 = models.ImageField(upload_to="gallery/", blank=True, null=True)
+	caption6 = models.CharField(max_length=120, blank=True)
+	updated = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		verbose_name = "גלריה"
+		verbose_name_plural = "גלריות"
+
+	def __str__(self):  # pragma: no cover
+		return self.title or f"Gallery #{self.pk}" 
